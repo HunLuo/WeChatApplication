705ffa5afdc02acdab23b5e98af782793bb58714
HunLuo <coder.mr.zhao@gmail.com>
2017-12-08 18:17:56 +0800

    upload

---
+++ MyFirstApplication/pages/index/index.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    second_height: 0,
+    score: 653,
+    array: [{
+      message: 'aaa',
+    }, {
+      message: 'bbb'
+    }, {
+      message: 'ccc'
+    }, {
+      message: 'ddd'
+    }, {
+      message: 'eee'
+    }, {
+      message: 'fff'
+    }]
+    
+  }
+
+})
